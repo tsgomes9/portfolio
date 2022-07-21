401cb120532ed0af85a2e9c808eebbc1baf2626f
Thiago Gomes <tsg.gomes9@gmail.com>
2022-07-21 10:22:08 -0400

    Estruturação da sessão de projetos

--- src/view/components/devProjects/devProjects.js
+++ src/view/components/devProjects/devProjects.js
@@ -1,21 +1,95 @@
 import './devProjects.scss'
+import mercadoCrypto from '../../img/mercadoCrypto.png'
 
 const DevProjects = () => {
   return (
     <div className="devProjects-session container" id="projects">
-      <h1 className="m-2">Projetos</h1>
+      <h1 className="">Projetos</h1>
 
-      <div className="row">
-        <div className="card">Projeto 1</div>
-      </div>
-      <div className="row">
-        <div className="card">Projeto 2</div>
-      </div>
-      <div className="row">
-        <div className="card">Projeto 3</div>
-      </div>
-      <div className="row">
-        <div className="card">Projeto 4</div>
+      <div className="row ">
+        <div className="col-12 col-sm-12 col-md-6 col-lg-4 ">
+          <div className="card p-4 project shadow-lg">
+            <h2>Mercado Crypto</h2>
+            <img
+              src={mercadoCrypto}
+              className="img-fluid rounded border mb-3"
+            ></img>
+            <p>
+              Projeto que traz informações em tempo real sobre o mercado de
+              criptomoedas. Consumindo a API do mercado bitcoin, são obtidos e
+              analisados os dados relacionados aos valores dos ativos. Para
+              garantir a responsividade, foi utilizado o framework Bootstrap. O
+              back end foi desenvolvido em javascript
+            </p>
+            <span class="material-symbols-outlined project-open">
+              open_in_new
+            </span>
+
+            <div className="project-icons p-2 rounded">
+              <i class="devicon-javascript-plain"></i>
+              <i class="devicon-html5-plain"></i>
+              <i class="devicon-css3-plain"></i>
+              <i class="devicon-bootstrap-plain"></i>
+              <i class="devicon-git-plain"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-sm-12 col-md-6 col-lg-4 ">
+          <div className="card p-4 project shadow-lg">
+            <h2>Mercado Crypto</h2>
+            <img
+              src={mercadoCrypto}
+              className="img-fluid rounded border mb-3"
+            ></img>
+            <p>
+              Projeto que traz informações em tempo real sobre o mercado de
+              criptomoedas. Consumindo a API do mercado bitcoin, são obtidos e
+              analisados os dados relacionados aos valores dos ativos. Para
+              garantir a responsividade, foi utilizado o framework Bootstrap. O
+              back end foi desenvolvido em javascript
+            </p>
+            <span class="material-symbols-outlined project-open">
+              open_in_new
+            </span>
+
+            <div className="project-icons p-2 rounded">
+              <i class="devicon-javascript-plain"></i>
+              <i class="devicon-html5-plain"></i>
+              <i class="devicon-css3-plain"></i>
+              <i class="devicon-bootstrap-plain"></i>
+              <i class="devicon-git-plain"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-sm-12 col-md-6 col-lg-4 ">
+          <div className="card p-4 project shadow-lg">
+            <h2>Mercado Crypto</h2>
+            <img
+              src={mercadoCrypto}
+              className="img-fluid rounded border mb-3"
+            ></img>
+            <p>
+              Projeto que traz informações em tempo real sobre o mercado de
+              criptomoedas. Consumindo a API do mercado bitcoin, são obtidos e
+              analisados os dados relacionados aos valores dos ativos. Para
+              garantir a responsividade, foi utilizado o framework Bootstrap. O
+              back end foi desenvolvido em javascript
+            </p>
+            <span class="material-symbols-outlined project-open">
+              open_in_new
+            </span>
+
+            <div className="project-icons p-2 rounded">
+              <i class="devicon-javascript-plain"></i>
+              <i class="devicon-html5-plain"></i>
+              <i class="devicon-css3-plain"></i>
+              <i class="devicon-bootstrap-plain"></i>
+              <i class="devicon-git-plain"></i>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/view/components/header/header.js
+++ src/view/components/header/header.js
@@ -2,61 +2,64 @@ import './headerStyle.scss'
 
 const Header = () => {
   return (
-    <div className="navbar bg-light fixed-top navbar-expand-lg" id="header">
-      <div className="container">
-        <a className="navbar-brand logo" href="#hello-world">
-          Meu Portfólio
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasNavbar"
-          aria-controls="offcanvasNavbar"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="offcanvas offcanvas-end"
-          tabindex="-1"
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-        >
-          <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-              Menu Principal
-            </h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#aboutMe">
-                  <div className="nav-style-item">Sobre mim</div>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#habilitys">
-                  <div className="nav-style-item">Habilidades</div>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#projects">
-                  <div className="nav-style-item">Projetos</div>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  <div className="nav-style-item-contact">Contato</div>
-                </a>
-              </li>
-            </ul>
-          </div>
+    <div
+      className="navbar bg-light fixed-top navbar-expand-lg px-4"
+      id="header"
+    >
+      {/* <div className="container"> */}
+      <a className="navbar-brand logo" href="#hello-world">
+        Meu Portfólio
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+            Menu Principal
+          </h5>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-body">
+          <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#aboutMe">
+                <div className="nav-style-item">Sobre mim</div>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#habilitys">
+                <div className="nav-style-item">Habilidades</div>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#projects">
+                <div className="nav-style-item">Projetos</div>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <div className="nav-style-item-contact">Contato</div>
+              </a>
+            </li>
+          </ul>
+          {/* </div> */}
         </div>
       </div>
     </div>
